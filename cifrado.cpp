319b05e919bba379e03b91355e75fc60f491329b
alvropz12-iwnl <alvropz12@gmail.com>
2025-11-25 11:30:42 -0600

    Ultimo cambio que le hicimos al codigo del examen final

--- cifrado.cpp
+++ cifrado.cpp
@@ -10,4 +10,36 @@ int main(){
     cin>>length;
     vector<char> phrase;
     vector<char> result;
+
+        for(int i=0;i<length;i++){
+        cout<<"enter the character "<<i+1<<'\n';
+        cin>> letra;
+        phrase.push_back(letra);
+        // Aplicar CIFRADO +2 / -1
+        if (letra >= 'a' && letra <= 'z') {
+
+            int index = letra - 'a';   // posición real en el abecedario
+
+            if (i % 2 == 0) {
+                // posiciones pares → +2
+                index = (index - 2+ 26) % 26;
+            } else {
+                // posiciones impares → -1
+                index = (index + 1) % 26;
+            }
+
+            result.push_back(abecedario[index]);
+
+        } else {
+            // si es espacio u otro caracter, lo ponemos igual
+            result.push_back(letra);
+        }
+    }
+
+        cout << "\nEncrypted result:\n";
+    for(char c : result) {
+        cout << c;
+    }
+    cout << endl;
+    
 }
