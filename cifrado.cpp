80c58ba8a15f1bde486cfff5a7ea2474966bdb20
alvropz12-iwnl <alvropz12@gmail.com>
2025-11-25 11:01:04 -0600

    Archivos del Proyecto

---
+++ cifrado.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    char abecedario[27] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+    int length;
+    char letra;
+    cout<<"Enter how many characters your phrase has: (count spaces as length 1)"<<endl;
+    cin>>length;
+    vector<char> phrase;
+    vector<char> result;
+}
